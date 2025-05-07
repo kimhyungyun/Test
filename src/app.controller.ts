8a04da6b0fc5e2cee67ea47d8a1770098a4e4965
somi9954 <jolh999@gmail.com>
2025-05-07 11:55:54 +0900

    second

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get()
+  @Get('test')
   getTest(): object {
     return { data: '테스트당', message: '테스트' };
   }
