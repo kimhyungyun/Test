0d7f0e72628ce400015631a874386d36e8c2a56e
somi9954 <jolh999@gmail.com>
2025-05-07 11:25:47 +0900

    test

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get()
+  getTest(): object {
+    return { data: '테스트당', message: '테스트' };
+  }
+  @Get()
+  getSiuuu(): object {
+    return { data: 'Siuuu', message: '씨.우' };
+  }
+}
